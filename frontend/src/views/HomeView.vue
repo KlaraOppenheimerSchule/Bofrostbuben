6dbbeb6ea4d8dba277612504828b39c7a70535a4
Benjamin Ruff <benjamin.ruff@flyeralarm.com>
2025-12-01 10:49:27 +0100

    feat: added views directory and boilerplate views for routing

---
+++ frontend/src/views/HomeView.vue
@@ -0,0 +1,4 @@
+<template> 
+    <h1>das hier ist die homepage</h1>
+    
+</template>
--- learning snippets/6-Routing.vue
+++ learning snippets/6-Routing.vue
@@ -16,4 +16,11 @@ The RouterView component tells Vue Router where to render the current route comp
 That's the component that corresponds to the current URL path. 
 
 The example above also uses {{ $route.fullPath }}.
-You can use $route in your component templates to access an object that represents the current route. -->
+You can use $route in your component templates to access an object that represents the current route.
+
+The view router is created in the router/index.ts file.
+The routes array defines the mapping between URL paths and components.
+
+in App.vue the component RouterView is used to display the component that corresponds to the current route.
+
+-->
