255bd8aca189972d397d78539532a3cb08489549
pascal <pascaldomine@t-online.de>
2025-12-02 14:16:29 +0100

    feat: Added a routerLink that navigates to historyView and dumbbell.svg; deleted a placeholder function

--- frontend/src/components/StreakTracker.vue
+++ frontend/src/components/StreakTracker.vue
@@ -1,21 +1,21 @@
 <script setup lang="ts">
 import { ref } from 'vue'
 
+//NOTE: The random number is only a placeholder for the streak later wich will be available from the backend
+
 function getRandomInt(max: number) {
   return Math.floor(Math.random() * max)
 }
 
 // generate the random number once
 const randomNumber = ref(getRandomInt(100))
-
-//the function will navigate to the history later
-function streakTracken(): void {
-  console.log("User would navigate to the history screen")
-}
 </script>
 
 <template>
-  <button @click="streakTracken">
-    {{ randomNumber }}
-  </button>
+  <RouterLink to="/history">
+    <button>
+      <img src="/assets/icons/dumbbell.svg" alt="Dumbbell" style="width: 25px; height: 25px; vertical-align: middle"/>
+      {{ randomNumber }}
+    </button>
+  </RouterLink>
 </template>
