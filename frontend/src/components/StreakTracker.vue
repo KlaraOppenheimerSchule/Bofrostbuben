ae33e63530964d59e71a6f408d7f8d96815df2b4
pascal <pascaldomine@t-online.de>
2025-12-02 10:54:23 +0100

    feat: new component "StreakTracker" that is a Button wich displays a random number. Later it will display the current streak. Right now on buttoncklick there is a console.log. Later it will navigate to a history-site

---
+++ frontend/src/components/StreakTracker.vue
@@ -0,0 +1,21 @@
+<script setup lang="ts">
+import { ref } from 'vue'
+
+function getRandomInt(max: number) {
+  return Math.floor(Math.random() * max)
+}
+
+// generate the random number once
+const randomNumber = ref(getRandomInt(100))
+
+//the function will navigate to the history later
+function streakTracken(): void {
+  console.log("User would navigate to the history screen")
+}
+</script>
+
+<template>
+  <button @click="streakTracken">
+    {{ randomNumber }}
+  </button>
+</template>
