4fa78a5e0d528cb719a94424d1f6f253ca295604
Elias Spiegel <elias.k.spiegel@gmail.com>
2025-12-03 11:33:22 +0100

    feat(navbar) added highlighting to active navbar tab, added SVG color filter to store

--- frontend/src/components/NavigationBar.vue
+++ frontend/src/components/NavigationBar.vue
@@ -1,29 +1,3 @@
-<script setup lang="ts"></script>
-
-<template>
-  <nav class="bottom-nav" :style="{ background: store.mainAppColor }">
-    <RouterLink to="/" class="nav-btn">
-      <img :src="homeIcon" class="nav-icon" />
-    </RouterLink>
-
-    <RouterLink to="/ExcerciseCatalogueView" class="nav-btn">
-      <img :src="catalogIcon" class="nav-icon" />
-    </RouterLink>
-
-    <RouterLink to="/CreatePlanView" class="nav-btn">
-      <img :src="newPlanIcon" class="nav-icon" />
-    </RouterLink>
-
-    <RouterLink to="/HistoryView" class="nav-btn">
-      <img :src="historyIcon" class="nav-icon" />
-    </RouterLink>
-
-    <RouterLink to="/SettingsView" class="nav-btn">
-      <img :src="settingsIcon" class="nav-icon" />
-    </RouterLink>
-  </nav>
-</template>
-
 <script setup lang="ts">
 import { store } from '@/stores/color_store'
 import homeIcon from '/assets/icons/home.svg'
@@ -33,43 +7,89 @@ import historyIcon from '/assets/icons/calendar.svg'
 import settingsIcon from '/assets/icons/settings.svg'
 </script>
 
+<template>
+  <nav class="bottom-nav" :style="{ background: store.mainAppColor }">
+    
+    <RouterLink to="/" v-slot="{ isActive }">
+      <button class="nav-btn" :class="{ active: isActive }">
+        <img :src="homeIcon" class="nav-icon" />
+      </button>
+    </RouterLink>
+
+    <RouterLink to="/ExcerciseCatalogueView" v-slot="{ isActive }">
+      <button class="nav-btn" :class="{ active: isActive }">
+        <img :src="catalogIcon" class="nav-icon" />
+      </button>
+    </RouterLink>
+
+    <RouterLink to="/CreatePlanView" v-slot="{ isActive }">
+      <button class="nav-btn" :class="{ active: isActive }">
+        <img :src="newPlanIcon" class="nav-icon" />
+      </button>
+    </RouterLink>
+
+    <RouterLink to="/HistoryView" v-slot="{ isActive }">
+      <button class="nav-btn" :class="{ active: isActive }">
+        <img :src="historyIcon" class="nav-icon" />
+      </button>
+    </RouterLink>
+
+    <RouterLink to="/SettingsView" v-slot="{ isActive }">
+      <button class="nav-btn" :class="{ active: isActive }">
+        <img :src="settingsIcon" class="nav-icon" />
+      </button>
+    </RouterLink>
+
+  </nav>
+</template>
+
 <style scoped>
 .bottom-nav {
   position: fixed;
-  bottom: 0.75rem; /* 12px */
+  bottom: 0.75rem;
   left: 50%;
   transform: translateX(-50%);
-  padding: 0.625rem; /* 10px */
+  padding: 0.625rem;
   display: flex;
   justify-content: center;
-  gap: 0.75rem; /* 12px */
-  border-radius: 2.5rem; /* 40px */
+  gap: 0.75rem;
+  border-radius: 2.5rem;
   width: max-content;
 }
 
-/* Buttons */
 .nav-btn {
-  width: 3.125rem; /* 50px */
-  height: 3.125rem; /* 50px */
+  width: 3.125rem;
+  height: 3.125rem;
   border-radius: 50%;
   border: none;
   background: rgba(255, 255, 255, 0.1);
   display: flex;
   justify-content: center;
   align-items: center;
+  transition: 0.2s;
 }
 
-.nav-btn:hover {
-  background: rgba(255, 255, 255, 0.2);
-}
-
-.nav-btn:active {
-  background: rgba(255, 255, 255, 0.05);
+.nav-btn.active {
+  background: white;
 }
 
 .nav-icon {
-  width: 1.5rem; /* 24px */
-  height: 1.5rem; /* 24px */
+  width: 1.5rem;
+  height: 1.5rem;
   filter: brightness(0) invert(1);
 }
+
+ .nav-btn:hover {
+    background-color: RGBA(255, 255, 255, 0.2);
+  }
+
+.nav-btn.active .nav-icon {
+  filter: v-bind('store.svgMainAppColorFilter');
+}
+
+ .nav-btn.active:hover {
+    background-color: white;
+  }
+
+
 </style>
