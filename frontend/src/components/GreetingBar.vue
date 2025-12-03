9673e5586a31506e38e6379c89e1c74d7f811146
pascal <pascaldomine@t-online.de>
2025-12-03 11:23:07 +0100

    feat: deletet placeholder in app.vue; alligned the greatingbar and edited the button

--- frontend/src/components/GreetingBar.vue
+++ frontend/src/components/GreetingBar.vue
@@ -10,10 +10,18 @@ function changeToProfile() {
 </script>
 
 <template>
-  <p>
-    <button @click="changeToProfile">
-      <img src="/assets/icons/muskelkater.svg" alt="Logo" style="" />
+  <p style="display: flex; align-items: center; gap: 0, 5rem; margin: 0">
+    <button
+      @click="changeToProfile"
+      style="all: unset; cursor: pointer; display: flex; align-items: center"
+    >
+      <img
+        src="/assets/icons/muskelkater.svg"
+        alt="Logo"
+        style="display: block; width: 5rem; height: 5rem"
+      />
     </button>
+
     Hallo, {{ UserName }}!
   </p>
 </template>
