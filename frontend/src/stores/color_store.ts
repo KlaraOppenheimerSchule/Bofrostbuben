4ed685665472c1e4fea64f2697c34932b0b31afa
Benjamin Ruff <benjamin.ruff@flyeralarm.com>
2025-12-04 14:58:33 +0100

    formatting

--- frontend/src/stores/color_store.ts
+++ frontend/src/stores/color_store.ts
@@ -2,5 +2,6 @@ import { reactive } from 'vue'
 
 export const store = reactive({
   mainAppColor: '#710002',
-  svgMainAppColorFilter: 'invert(20%) sepia(34%) saturate(7310%) hue-rotate(346deg) brightness(48%) contrast(115%)'
+  svgMainAppColorFilter:
+    'invert(20%) sepia(34%) saturate(7310%) hue-rotate(346deg) brightness(48%) contrast(115%)',
 })
