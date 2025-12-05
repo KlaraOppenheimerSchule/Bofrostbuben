2c3278f2e5f133256dfb32871c02ad9fa4681d5c
pascal <pascaldomine@t-online.de>
2025-12-05 09:53:25 +0100

    feat: incomplete changes

---
+++ frontend/src/components/Buttons/ShowWorkoutPlanButton.vue
@@ -0,0 +1,21 @@
+<script setup lang="ts">
+import Button from '../Buttons/Button.vue'
+
+</script>
+
+<template>
+  <Button title1="Show" title2="Workout Plan"></Button>
+</template>
+
+<style scoped>
+.start-free-workout-btn::before {
+  content: '';
+  display: block;
+  width: 0;
+  height: 0;
+  border-top: 2rem solid transparent;
+  border-bottom: 2rem solid transparent;
+  border-left: 3rem solid white;
+  margin-bottom: 0.8rem;
+}
+</style>
---
+++ frontend/src/views/FreeWorkoutView.vue
@@ -0,0 +1,6 @@
+<script setup lang="ts">
+</script>
+
+<template>
+  <h1>here you wil be able to track free workouts</h1>
+</template>
