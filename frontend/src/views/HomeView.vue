228acccd66b78feec7e93f993e3a559718466d24
Dominic Glück <Dominic.Glueck@flyeralarm.com>
2025-12-08 10:39:16 +0100

    fixed homeview components

--- frontend/src/views/HomeView.vue
+++ frontend/src/views/HomeView.vue
@@ -1,7 +1,11 @@
 <script setup lang="ts">
 import StartWorkoutButton from '../components/StartWorkoutButton.vue'
+import GreetingBar from "@/components/GreetingBar.vue";
+import StreakTracker from "@/components/StreakTracker.vue";
 </script>
 
 <template>
-  <StartWorkoutButton> </StartWorkoutButton>
+  <greeting-bar/>
+  <streak-tracker/>
+  <StartWorkoutButton/>
 </template>
