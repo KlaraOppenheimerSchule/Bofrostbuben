7d44e4cf179c468ac142f061b39a4105c87586ef
pascal <pascaldomine@t-online.de>
2025-12-09 11:07:53 +0100

    feat: the profilepicture routes to the ProfileView

--- frontend/src/components/GreetingBar.vue
+++ frontend/src/components/GreetingBar.vue
@@ -4,18 +4,17 @@ import { ref } from 'vue'
 const names = ['Benjamin Nethanyahu', 'Charlie', 'Fortnite', 'SixSeven']
 const UserName = ref(names[Math.floor(Math.random() * names.length)])
 
-function changeToProfile() {
-  alert('route to profile')
-}
 </script>
 
 <template>
-  <p class="greeting-container">
-    <button class="icon-button" @click="changeToProfile">
-      <img src="/assets/icons/muskelkater.svg" alt="Logo" class="icon-img" />
-    </button>
+  <div class="greeting-container">
+    <RouterLink to="/ProfileView" class="link-no-decoration">
+      <button class="icon-button">
+        <img src="/assets/icons/muskelkater.svg" alt="Logo" class="icon-img" />
+      </button>
+    </RouterLink>
     Hallo, {{ UserName }}!
-  </p>
+  </div>
 </template>
 
 <style scoped>
