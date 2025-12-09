ac02f6420021337a952289febff545113ef0487c
Dominic Glück <Dominic.Glueck@flyeralarm.com>
2025-12-09 14:55:56 +0100

    added options component

--- frontend/src/views/StartWorkoutView.vue
+++ frontend/src/views/StartWorkoutView.vue
@@ -1,12 +1,11 @@
 <script setup lang="ts">
 import { ref } from 'vue'
-import CreateWorkoutPlanButton from '@/components/buttons/CreateWorkoutPlanButton.vue';
-import FreeWorkoutButton from '@/components/buttons/StartFreeWorkoutButton.vue';
-import ShowWorkoutPlanButton from '@/components/buttons/ShowWorkoutPlanButton.vue';
+import CreateWorkoutPlanButton from '@/components/buttons/CreateWorkoutPlanButton.vue'
+import FreeWorkoutButton from '@/components/buttons/StartFreeWorkoutButton.vue'
+import ShowWorkoutPlanButton from '@/components/buttons/ShowWorkoutPlanButton.vue'
 
 //This is a conditional const; if it is true the CreateWorkoutPlanButton is displayed, otherwise the ShowWorkoutPlanButton
 const showCreatePlan = ref(true)
-
 </script>
 
 <template>
