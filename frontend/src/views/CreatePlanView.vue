15b2eebc63a27878c11be4751df5f36ec1767378
pascal <pascaldomine@t-online.de>
2025-12-10 09:02:52 +0100

    feat: Greetingbar included to different views

--- frontend/src/views/CreatePlanView.vue
+++ frontend/src/views/CreatePlanView.vue
@@ -1,5 +1,8 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import GreetingBar from '@/components/GreetingBar.vue'
+</script>
 
 <template>
+  <GreetingBar/>
   <h1>Create your Plan here!</h1>
 </template>
--- frontend/src/views/SettingsView.vue
+++ frontend/src/views/SettingsView.vue
@@ -1,7 +1,9 @@
 <script setup lang="ts">
 import Option from '@/components/Option.vue'
+import GreetingBar from '@/components/GreetingBar.vue';
 </script>
 
 <template>
+  <GreetingBar />
   <Option />
 </template>
